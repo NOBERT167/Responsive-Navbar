d8fa247c2939c76c22b2369d80b32bc48166a124
debapps <104157238+debapps@users.noreply.github.com>
2022-04-25 10:57:27 +0000

    Deploying to gh-pages from @ debapps/Responsive-Navbar@4e736fd224db93fd93de8a4573bc1b482404b359 🚀

---
+++ navOps.js
@@ -0,0 +1,28 @@
+let collapseLink = document.querySelector("#collapseLink");
+let navLinks = document.querySelectorAll("#collapseLink li");
+let menuIcon = document.querySelector("#menuIcon");
+let crossIcon = document.querySelector("#crossIcon");
+
+
+// Toggle Menu Icon and Cross Icons alternatively.
+function toggleIcons () {
+    menuIcon.classList.toggle("hidden");
+    crossIcon.classList.toggle("hidden");
+    collapseLink.classList.toggle("hidden");
+}
+
+// Make the navigation links hidden when a navlink is clicked.
+function makeHiddenNav () {
+    if (collapseLink.classList.contains("absolute")) {
+        collapseLink.classList.add("hidden");
+    }
+    menuIcon.classList.toggle("hidden");
+    crossIcon.classList.toggle("hidden");
+}
+
+// Attaching the listeners.
+menuIcon.addEventListener("click", toggleIcons);
+crossIcon.addEventListener("click", toggleIcons);
+navLinks.forEach((nav) => {
+    nav.addEventListener("click", makeHiddenNav);
+});
