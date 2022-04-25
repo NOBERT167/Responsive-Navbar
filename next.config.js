9a3614a42aa9251994f5d35b88ccf31746d56fbe
Debaditya Bhar <debaditya.bhar@outlook.com>
2022-04-25 16:37:42 +0530

    Added basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   images: {
     loader: "akamai",
     path: ""
-  }
+  },
+  
+  basePath: "/Responsive-Navbar",
+  assetPrefix: "/Responsive-Navbar", 
 }
 
 module.exports = nextConfig
